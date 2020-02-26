cd51b70091d0f53ccb1ad78e8f5373c3b41106f1
Jordi Aragón Zaragoza <jordiaragonzaragoza@gmail.com>
2020-02-26 11:22:37 +0100

    Passing and calling delegates in other class.

--- CsharpPlayground/1.4 DelegatesAndEvents/LearningDelegates.cs
+++ CsharpPlayground/1.4 DelegatesAndEvents/LearningDelegates.cs
@@ -2,8 +2,6 @@
 
 namespace LearningDelegates
 {
-    //Delegates allow send a method as parameter in other classes
-
     //Declare a delegate. It is not required inside a class.
     public delegate void PrintDelegate(string value);
 
@@ -12,6 +10,8 @@ namespace LearningDelegates
 
     public static class LearningDelegates
     {
+        public delegate bool BoolDelegate(string text, int number);
+
         public static void Start()
         {
             InstantiateDelegate();
@@ -20,7 +20,16 @@ namespace LearningDelegates
             DelegateUsingActionAnonymous();
             DelegateUsingFunc();
             DelegateUsingPredicate();
+
+            UsingOtherClasses();
+
             Console.ReadLine();
+        }
+
+        public static bool AlwaysTrueConsolePrint(string text, int number)
+        {
+            Console.WriteLine($"Hello. I'm AlwaysTrueConsolePrint() Method. This are my parameters: text: {text}, number: {number}");
+            return true;
         }
 
         //Same input and output as delegate to allow instantiate throw delegate.
@@ -39,6 +48,9 @@ namespace LearningDelegates
         {
             var printDelegate = new PrintDelegate(ConsolePrint);
             printDelegate("I'm a simple delegate.");
+
+            var trueDelegate = new BoolDelegate(AlwaysTrueConsolePrint);
+            var returnedValue = trueDelegate("sample text", 100);
         }
 
         public static void InstantiateDelegateGenerics()
@@ -53,13 +65,11 @@ namespace LearningDelegates
 
         public static void DelegateUsingAction()
         {
-            //Delegates Action always return void. Allows to 16 parameters.
+            //Action always return void. Allows to 16 parameters.
             Action<string> consolePrintAction = ConsolePrint;
             consolePrintAction("I'm an action.");
-            
-            OtherClass.SampleMethod(consolePrintAction);
         }
-
+        
         public static void DelegateUsingActionAnonymous()
         {
             //Now using a new anonymous method to print on console.
@@ -104,13 +114,62 @@ namespace LearningDelegates
             Console.WriteLine(isAdult ? "is adult": "is not adult");
         }
 
+        public static void UsingOtherClasses()
+        {
+            //Passing Delegate
+            var trueDelegate = new BoolDelegate(AlwaysTrueConsolePrint);
+            var boolDelegateReturnedValue = OtherClass.PassingDelegate(trueDelegate);
+
+            //Passing Action
+            Action<string> consolePrintAction = ConsolePrint;
+            OtherClass.PassingAction(consolePrintAction);
+
+            //Passing Func. Output string
+            Func<string, string> consolePrintAnonymousFunc = delegate (string value)
+            {
+                Console.WriteLine(value);
+                return string.Empty;
+            };
+
+            var funcReturnedValue = OtherClass.PassingFunc(consolePrintAnonymousFunc);
+
+            //Passing Predicate
+            Predicate<int> adult = value => value > 17;
+            var predicateReturnedValue = OtherClass.PassingPredicate(adult);
+
+        }
     }
 
+    //Delegates allow send a method as parameter in other classes
     public static class OtherClass
     {
-        public static void SampleMethod(Action<string> action)
+        public static bool PassingDelegate(LearningDelegates.BoolDelegate @delegate)
+        {
+            bool returnedValue = @delegate("sample text", 100);
+            //Do some logic...
+            Console.WriteLine("Im a delegate executed from other class!");
+            return true;
+        }
+    
+        public static void PassingAction(Action<string> action)
         {
             action("Im an action executed from other class!");
+            //Do some logic...
+        }
+
+        public static string PassingFunc(Func<string, string> @func)
+        {
+            string returnedValue = @func("I'm a func using an anonymous method from other class!");
+            //Do some logic...
+            return string.Empty;
+        }
+
+        public static bool PassingPredicate(Predicate<int> @predicate)
+        {
+            bool returnedValue = @predicate(25);
+            //Do some logic...
+            Console.WriteLine("Im a predicate executed from other class!");
+            return true;
         }
     }
 }
